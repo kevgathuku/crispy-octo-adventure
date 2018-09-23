592b8be97cbd941de604031b8c532d8058b8d92e
Kevin Ndung'u <kevgathuku@gmail.com>
2018-09-24 00:29:24 +0300

    Add merge sort in Elixir

--- algos_ex/lib/algos_ex.ex
+++ algos_ex/lib/algos_ex.ex
@@ -1,24 +1,8 @@
 defmodule Algos do
-  @moduledoc """
-  Documentation for Algos.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Algos.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
+  # Selection sort
   @spec selection_sort(list) :: list
   def selection_sort(lst) when is_list(lst) do
-    # Transform to a function that takes 2 args i.e. head, acc
+    # Transform to a function that takes 2 args i.e. lst, acc
     do_selection(lst, [])
   end
 
@@ -33,5 +17,32 @@ defmodule Algos do
   def do_selection(lst, acc) do
     min = Enum.min(lst)
     do_selection(:lists.delete(min, lst), acc ++ [min])
+  end
+
+  def split(list) do
+    len = (length(list) / 2) |> round
+    Enum.split(list, len)
+  end
+
+  # Merge sort
+  @spec merge_sort(list) :: list
+  def merge_sort([]), do: []
+  def merge_sort([x]), do: [x]
+
+  def merge_sort(lst) do
+    {left, right} = split(lst)
+    merge(merge_sort(left), merge_sort(right), [])
+  end
+
+  # if right is empty, add all elements from the left
+  def merge(left, [], acc), do: acc ++ left
+  def merge([], right, acc), do: acc ++ right
+
+  def merge(left = [left_head | left_tail], right = [right_head | right_tail], acc) do
+    cond do
+      # Add smallest element to end of accumulator
+      left_head < right_head -> merge(left_tail, right, acc ++ [left_head])
+      true -> merge(left, right_tail, acc ++ [right_head])
+    end
   end
 end
--- algos_ex/test/algos_ex_test.exs
+++ algos_ex/test/algos_ex_test.exs
@@ -1,17 +1,54 @@
-defmodule AlgosTest do
+defmodule SelectionSortTests do
   use ExUnit.Case
   use Quixir
-  doctest Algos
-
-  test "greets the world" do
-    assert Algos.hello() == :world
-  end
 
   # Selection sort
   test "sorts unsorted numbers" do
     lst = [19, 64, 5, 9, 12]
     expected = [5, 9, 12, 19, 64]
     assert Algos.selection_sort(lst) == expected
+  end
+
+  test "sorts lists with any type" do
+    ptest original: list() do
+      assert Algos.selection_sort(original) == Enum.sort(original)
+    end
+  end
+
+  test "a sorted list has the same length as the original" do
+    ptest original: list() do
+      sorted = Algos.selection_sort(original)
+      assert length(sorted) == length(original)
+    end
+  end
+
+  test "sorting a list of length 1 does nothing" do
+    ptest original: list(1) do
+      assert Algos.selection_sort(original) == original
+    end
+  end
+end
+
+defmodule MergeSortTests do
+  use ExUnit.Case
+  use Quixir
+
+  test "split generates sublists as expected" do
+    {left, right} = Algos.split([1, 2, 3, 4, 5])
+    assert [1, 2, 3] == left
+    assert [4, 5] == right
+  end
+
+  # Merge sort
+  test "Leaves already sorted list as is" do
+    sorted = Algos.merge_sort([1, 2, 3, 4, 5])
+    assert [1, 2, 3, 4, 5] == sorted
+  end
+
+  test "sorts unsorted numbers" do
+    lst = [19, 64, 5, 9, 12]
+    expected = [5, 9, 12, 19, 64]
+    assert Algos.merge_sort(lst) == expected
   end
 
   test "sorts lists with any type" do
